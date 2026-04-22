89e36a550f4edeb70b4f2a6e06aa5c157b70b2b9
AmanKumarJangid <amanjangid7847@gmail.com>
2026-04-22 22:06:38 +0530

    Origin and cors update method

--- server.js
+++ server.js
@@ -16,10 +16,21 @@ connectDB();
 // Security
 app.use(helmet());
 
+// 1. Get origins from env, split by comma, and trim any accidental spaces
+const allowedOrigins = (process.env.FRONTEND_URL || "http://localhost:3000")
+  .split(",")
+  .map(origin => origin.trim());
 // CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: (origin , callback ) => {
+      if( !origin || allowedOrigins.includes(origin)){
+        callback(null, true);
+      }
+      else{
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
